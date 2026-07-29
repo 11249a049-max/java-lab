9c7712a8521ba094c92f6487409eb249bfc35f2f
11249a049-max <11249a049@kanchiuniv.ac.in>
2026-07-29 10:31:36 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String name="Haritha";
+    int age=19;
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+    public static void main(String[] args){
+        Student s=new Student();
+        s.display();
+    }
+}
---
+++ VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo{
+int instanceVar=10;
+static String staticVar="I am static";
+public void showVariables()
+{
+int localVar=5;
+System.out.println("Instance Variable:"+instanceVar);
+System.out.println("Local Variable:"+localVar);
+System.out.println("Static Variable:"+staticVar);
+}
+public static void main(String[] args)
+{
+VariablesDemo obj1=new VariablesDemo();
+obj1.showVariables();
+System.out.println("Accessing Static Variable via class:"+VariablesDemo.staticVar);
+}
+}
