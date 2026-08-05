94aa762897242217eeaf46d22d17c4dbd3727d36
11249a049-max <11249a049@kanchiuniv.ac.in>
2026-08-05 11:56:33 +0530

    Add files via upload

---
+++ FinalDemo.java
@@ -0,0 +1,6 @@
+class FinalDemo {
+    public static void main(String[] args) {
+        final int MAX = 100;
+         System.out.println("The maximum value is: " + MAX);
+    }   
+}
---
+++ StaticDemo.java
@@ -0,0 +1,9 @@
+public class StaticDemo {
+    static void display(){
+        System.out.println("This is a static method.");
+    }
+    public static void main(String[] args){
+        StaticDemo.display();
+    }
+    
+}
